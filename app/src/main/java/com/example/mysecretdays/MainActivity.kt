280a19f88a2fb698ebef7281f6ce50cfae592556
Dowon Kim <dowoniscool@yahoo.com>
2023-11-02 17:43:06 +0100

    Feat : Add function to access the combination and change of the combination

--- app/src/main/java/com/example/mysecretdays/MainActivity.kt
+++ app/src/main/java/com/example/mysecretdays/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.mysecretdays
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private val changePasswordButton: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.changePasswordButton)
     }
+
+    private var changePasswordMode = false
 
     private val firstNumberPicker: NumberPicker by lazy {
         findViewById<NumberPicker>(R.id.firstNumberPicker)
@@ -42,6 +47,16 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    private fun showErrorAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("FAILED!!!")
+            .setMessage("WRONG CODE!")
+            .setPositiveButton("Confirm") { _, _ -> }
+            .create()
+            .show()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -51,8 +66,19 @@ class MainActivity : AppCompatActivity() {
         thirdNumberPicker
 
         openLockButton.setOnClickListener {
+
+            if (changePasswordMode) {
+                Toast.makeText(
+                    this,
+                    "During change of password, you can't open it!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
             val passwordPreference = getSharedPreferences("password", Context.MODE_PRIVATE)
-            val passwordFromUser = "${firstNumberPicker.value}${secondNumberPicker.value}${thirdNumberPicker.value}"
+            val passwordFromUser =
+                "${firstNumberPicker.value}${secondNumberPicker.value}${thirdNumberPicker.value}"
 
             if (passwordPreference.getString("password", "000").equals(passwordFromUser)) {
                 // Access authorised!
@@ -61,12 +87,34 @@ class MainActivity : AppCompatActivity() {
             } else {
                 // Access Failed!
                 //TODO -> error pops up
-                AlertDialog.Builder(this)
-                    .setTitle("FAILED!!!")
-                    .setMessage("WRONG CODE!")
-                    .setPositiveButton("Confirm") { _, _ -> }
-                    .create()
-                    .show()
+                showErrorAlertDialog()
+            }
+        }
+
+        changePasswordButton.setOnClickListener {
+            val passwordPreference = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser =
+                "${firstNumberPicker.value}${secondNumberPicker.value}${thirdNumberPicker.value}"
+
+
+            if (changePasswordMode) {
+                //SAVE the combination
+                passwordPreference.edit(true) {
+                    putString("password", passwordFromUser)
+                }
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+            } else {
+                //ACTIVATE changePasswordMode :: check if the combination's correct or not (without verification, don't let the user change the comb)
+                if (passwordPreference.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "Create new combination", Toast.LENGTH_SHORT).show()
+                    changePasswordButton.setBackgroundColor(Color.RED)
+                } else {
+                    // Access Failed!
+                    //TODO -> error pops up
+                    showErrorAlertDialog()
+                }
             }
         }
 
